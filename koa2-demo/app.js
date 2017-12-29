b9373068366021ad31b7a1cd525d9ab75067d26d
imeetmx <imeetmx@qq.com>
2017-12-29 16:07:48 +0800

    add app.js bodyparse

--- koa2-demo/app.js
+++ koa2-demo/app.js
@@ -3,6 +3,7 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
+const convert = require('koa-convert');
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-session');
@@ -15,12 +16,11 @@ const index = require('./routes/index')
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-//app.use(require('koa-static')(__dirname + '/public'))
+// middlewares
+app.use(convert(bodyparser));
+app.use(convert(json()));
+app.use(convert(logger()));
+
 
 app.keys = ['X-Ui'];
 
