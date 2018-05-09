5baea8d966ace4adec82e2c4368fa68c47815ae4
imeetmx <nodeweb@x-ui.com>
2018-05-09 18:25:48 +0800

    rm src

--- reactWeb/src/components/loading.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Loading() {
-    return (
-        <div>加载中.......</div>
-    )
-}
--- reactWeb/src/components/notfound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-const Home = () => (
-    <div>404 NOT FOUND!</div>
-)
-
-export default Home;
