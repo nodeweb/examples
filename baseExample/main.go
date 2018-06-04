8eeab8911a79b1f4900b54b946b00bdfd9076c32
imeetmx <nodeweb@x-ui.com>
2018-06-04 12:52:26 +0800

    add go-redis

--- baseExample/main.go
+++ baseExample/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"baseExample/routers"
 	"baseExample/mysqlConf"
+	"baseExample/redisConf"
 )
 
 func main(){
 	db := mysqlConf.Mydb
+	redb := redisConf.Client
 	defer db.Close()
+	defer redb.Close()
 
 	router := routers.RouterInit()
 	router.Run()
